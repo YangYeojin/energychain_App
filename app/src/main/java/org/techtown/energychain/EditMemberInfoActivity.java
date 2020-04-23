fa374e620bcedbabe187c109542a771db35f7161
yangyeojin <yangyeojin79@gmail.com>
2020-04-23 22:59:09 +0900

    232200 change colors, Edit members info 232258

---
+++ app/src/main/java/org/techtown/energychain/EditMemberInfoActivity.java
@@ -0,0 +1,60 @@
+package org.techtown.energychain;
+
+import android.app.Activity;
+import android.app.AppComponentFactory;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditMemberInfoActivity extends AppCompatActivity {
+
+    EditText eif_id_editText, eif_name_editText, eif_pw_EditText, eif_pwcheck_editText, eif_account_editText;
+    ImageView eif_pw_check_red, eif_pw_check_green;
+
+    private ArrayAdapter adapter;
+    private Spinner spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_member_info);
+
+        // change and check password
+        eif_pw_EditText = (EditText)findViewById(R.id.eif_pw_EditText);
+        eif_pwcheck_editText = (EditText)findViewById(R.id.eif_pwcheck_editText);
+
+        eif_pwcheck_editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+            /* 2020.04.23.01:04 can not password check T_T image file not inserted!! if else R.drawable check */
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (eif_pw_EditText.getText().toString().equals(eif_pwcheck_editText.getText().toString())) {
+                    eif_pw_check_green.setImageResource(R.drawable.ic_check_green_40dp);
+                } else {
+                    eif_pw_check_red.setImageResource(R.drawable.ic_cancel_red_40dp);
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        spinner = (Spinner)findViewById(R.id.OrgSpinner);
+        adapter = ArrayAdapter.createFromResource(this, R.array.Org, android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
+
+}

41df0dacf597c0b85bb7f34a8e8dd3f220141b57
ROHKW <elcid2314@gmail.com>
2020-04-23 16:53:22 +0900

    some changed

---
+++ app/src/main/java/org/techtown/energychain/mm11.java
@@ -0,0 +1,14 @@
+package com.example.mypage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mm11 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mm11);
+    }
+}

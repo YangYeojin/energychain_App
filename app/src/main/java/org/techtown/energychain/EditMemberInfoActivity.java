562b672727285a0d1112b70a1c19194e3490789d
yangyeojin <yangyeojin79@gmail.com>
2020-04-26 23:29:29 +0900

    yeoin 261127 edit_button, charge_recharge, edit_info

--- app/src/main/java/org/techtown/energychain/EditMemberInfoActivity.java
+++ app/src/main/java/org/techtown/energychain/EditMemberInfoActivity.java
@@ -1,59 +1,93 @@
 package org.techtown.energychain;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.Spinner;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EditMemberInfoActivity extends AppCompatActivity {
 
-    EditText eif_id_TextView, eif_name_TextView, eif_pw_EditText, eif_pwcheck_editText, eif_account_editText;
-    ImageView eif_pw_check_red, eif_pw_check_green;
-
-    private ArrayAdapter adapter;
-    private Spinner spinner;
+    //private ArrayAdapter adapter;
+    //private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_member_info);
 
-        // change and check password
-        eif_pw_EditText = (EditText)findViewById(R.id.eif_pw_EditText);
-        eif_pwcheck_editText = (EditText)findViewById(R.id.eif_pwcheck_editText);
-        eif_pw_check_green = (ImageView)findViewById(R.id.eif_pw_check_green);
-        eif_pw_check_red = (ImageView)findViewById(R.id.eif_pw_check_red);
+        final Button energymainButton = (Button)findViewById(R.id.energymainButton);
+        final Button purchaseButton = (Button)findViewById(R.id.purchaseButton);
+        final Button saleButton = (Button)findViewById(R.id.saleButton);
+        final Button mydataButton = (Button)findViewById(R.id.mydataButton);
 
-        eif_pwcheck_editText.addTextChangedListener(new TextWatcher() {
+        energymainButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
-            }
-            // 2020.04.23.01:04 can not password check T_T image file not inserted!! if else R.drawable check
-            @Override
-            public void onTextChanged(CharSequence s, int start, int count, int after) {
-                String password = eif_pw_EditText.getText().toString();
-                String confirm = eif_pwcheck_editText.getText().toString();
-
-                if (password.equals(confirm)) {
-                    eif_pw_check_green.setImageResource(R.drawable.ic_check_green_40dp);
-                } else {
-                    eif_pw_check_red.setImageResource(R.drawable.ic_cancel_red_40dp);
-                }
-            }
-            @Override
-            public void afterTextChanged(Editable editable) {
-
+            public void onClick(View v) {
+                Intent mainIntent = new Intent(EditMemberInfoActivity.this, MainActivity.class);
+                startActivity(mainIntent);
             }
         });
-        spinner = (Spinner)findViewById(R.id.OrgSpinner);
-        adapter = ArrayAdapter.createFromResource(this, R.array.Org, android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+
+        purchaseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainIntent = new Intent(EditMemberInfoActivity.this, Purchase.class);
+                startActivity(mainIntent);
+            }
+        });
+
+
+        saleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent mainIntent = new Intent(EditMemberInfoActivity.this, Sale.class);
+                startActivity(mainIntent);
+            }
+        });
+
+
+        mydataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainIntent = new Intent(EditMemberInfoActivity.this, MyPage.class);
+                startActivity(mainIntent);
+            }
+        });
+
     }
 
+    // logic of edit_button
+    public void onEditButtonClicked(View v) {
+        EditText eif_pw_EditText = (EditText)findViewById(R.id.eif_pw_EditText);
+        EditText eif_pwcheck_editText = (EditText)findViewById(R.id.eif_pwcheck_editText);
+        EditText eif_EmailText = (EditText) findViewById(R.id.eif_EmailText);
 
+        String eif_pw_String, eif_pwcheck_String, eif_Email_String;
+
+        eif_pw_String = eif_pw_EditText.getText().toString();
+        eif_pwcheck_String = eif_pwcheck_editText.getText().toString();
+        eif_Email_String = eif_EmailText.getText().toString();
+
+        if (eif_pw_String.equals(eif_pwcheck_String)) {
+            Toast.makeText(getApplicationContext(), "개인정보가 변경되었습니다.", Toast.LENGTH_SHORT).show();
+            if (eif_Email_String.matches("")){
+            } else {
+                // code for changing email on the server
+
+            }
+        } else {
+            Toast.makeText(getApplicationContext(), "비밀번호를 확인해주세요.", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
